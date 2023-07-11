af1857378c7a1cc99c0e4484aaf6643c0962007c
huangxkatherine <139277189+huangxkatherine@users.noreply.github.com>
2023-07-11 16:04:26 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,9 +15,26 @@ def load_lottieurl(url):
 lottie = "https://lottie.host/58fb7ece-b89d-4757-ba79-db51a7842646/LWQp5WwdiE.json"
 
 # -- HEADER SECTION --
-
+    
 with st.container():
-
+    
         st.subheader("Fusion Education")
         st.title("To-Do List")
-"app.py" 40L, 830C
+        st.write("[Learn More](https://www.fusionedus.com)")
+
+# -- LIST OF ACTIVITIES --
+
+with st.container():
+        st.write("---")
+        left_column, right_column = st. columns(2)
+        with left_column:
+                st. header("Activities to be completed:")
+                st.write(
+                        """
+                        - Activity 1
+                        - Activity 2
+                        - Activity 3
+                        """
+                )
+        with right_column:
+                st_lottie(lottie, height=300, key="to do")
